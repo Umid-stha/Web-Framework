a0359562982d45b1b8c0a8f70a405ddca3564a98
Umid Shrestha <137206081+Umid-stha@users.noreply.github.com>
2024-10-15 04:57:06 +0000

    trying to add routing1

--- mango/api.py
+++ mango/api.py
@@ -1,15 +1,48 @@
 from webob import Request, Response
+from parse import parse
 
 class API:
+    def __init__(self):
+        self.routes = {}
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
 
-    def handle_request(self, request):
-        user_agent = request.environ.get("HTTP_USER_AGENT", "No User Agent Found")
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+        return wrapper
 
+    def handle_request(self, request):
         response = Response()
-        response.text = f"Hello, my friend with this user agent: {user_agent}"
+        try:
+            handler, name = self.find_handler(request)
+        finally:
+            handler = self.find_handler(request)
+        if handler is not None:
+            if name:
+                handler(request, response, name)
+            handler(request, response)
+        else:
+            self.defaultResponse(response)
 
         return response
+    
+    def find_handler(self, request):
+        for path, handler in self.routes.items():
+            if path == request.path:
+                try:
+                    para1 = path.split("/")[:1]
+                    if para1 == "hello":
+                        result = parse('/hello/{name}', path)
+                        name = result.named['name']
+                        return handler, name
+                finally: 
+                    return handler
+    
+    def defaultResponse(self, response):
+        response.status_code = 404
+        response.text = "Page not found"
--- mango/app.py
+++ mango/app.py
@@ -1,3 +1,15 @@
 from api import API
 
 app=API()
+
+@app.route("/")
+def home(request, response):
+    response.text = "welcome to the home page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "welcome to the about page"
+
+@app.route('/hello/{name}')
+def say_hello(request, response, name):
+    response.text = f"hello, {name}"
