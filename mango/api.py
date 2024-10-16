c88adb22bd062496145a1c35a04bcf87bd337bee
Umid Shrestha <137206081+Umid-stha@users.noreply.github.com>
2024-10-16 04:23:28 +0000

    Duplicate Routes and Class Based Handlers

--- mango/api.py
+++ mango/api.py
@@ -1,5 +1,6 @@
 from webob import Request, Response
 from parse import parse
+import inspect
 
 class API:
     def __init__(self):
@@ -12,37 +13,50 @@ class API:
 
     def route(self, path):
         def wrapper(handler):
+            for existing_path, existing_handler in self.routes.items():
+                if path == existing_path:
+                    raise AssertionError("Same path can't be repeated for diffrent functions")
             self.routes[path] = handler
             return handler
         return wrapper
 
     def handle_request(self, request):
         response = Response()
-        try:
-            handler, name = self.find_handler(request)
-        finally:
-            handler = self.find_handler(request)
+        handler, kwargs = self.find_handler(request)
+        print(handler, kwargs)
         if handler is not None:
-            if name:
-                handler(request, response, name)
-            handler(request, response)
+            if inspect.isClass(handler):
+                handler_func = getattr(handler(), request.method.lower(), None)
+            else:
+                handler(request, response, **kwargs)
         else:
             self.defaultResponse(response)
-
         return response
     
+    # own solution
+    # def find_handler(self, request):
+    #     for path, handler in self.routes.items():
+    #         para1 = path.split("/")[1:2]
+    #         print(para1)
+    #         if para1 == "hello":
+    #             result = parse('/hello/{name}', path)
+    #             name = result.named['name']
+    #             return handler, name
+    #         else:
+    #             if path == request.path:
+    #                 print(path)
+    #                 return handler, None
+    #         return None, None
+
+    #tutorial
     def find_handler(self, request):
         for path, handler in self.routes.items():
-            if path == request.path:
-                try:
-                    para1 = path.split("/")[:1]
-                    if para1 == "hello":
-                        result = parse('/hello/{name}', path)
-                        name = result.named['name']
-                        return handler, name
-                finally: 
-                    return handler
-    
+            result = parse(path, request.path)
+            if result is not None:
+                name = result.named
+                return handler, name
+        return None, None 
+        
     def defaultResponse(self, response):
         response.status_code = 404
         response.text = "Page not found"
--- mango/app.py
+++ mango/app.py
@@ -6,10 +6,26 @@ app=API()
 def home(request, response):
     response.text = "welcome to the home page"
 
+# @app.route("/")
+# def home2(request, response):
+#     response.text = "welcome to the home page 2"
+
 @app.route("/about")
 def about(request, response):
     response.text = "welcome to the about page"
 
-@app.route('/hello/{name}')
+@app.route("/hello/{name}")
 def say_hello(request, response, name):
     response.text = f"hello, {name}"
+
+@app.route("/student/{id:d}")
+def student(request, response, id):
+    response.text = f"This students Roll no. is {id}"
+
+@app.route("/book")
+class BooksResource:
+    def get(self, req, resp):
+        resp.text = "Books Page"
+
+    def post(self, req, resp):
+        resp.text = "Endpoint to create a book"
